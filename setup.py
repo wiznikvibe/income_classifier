83c24c37abc7418a5c84db2e2ce266e2c8a40d0b
wiznikvibe <nikhilshetty439@gmail.com>
2023-10-19 20:49:00 +0530

    Project Template and Setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements(file_path:str)-> List[str]:
+    requirements = []
+    HYPEN_DOT_E = "-e ."
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n','') for req in requirements]
+        if HYPEN_DOT_E in requirements:
+            requirements.remove(HYPEN_DOT_E)
+
+    
+    return requirements
+
+
+setup(
+    name="income_classifier",
+    version="0.0.1",
+    description="Income Classification",
+    author="Nikhil",
+    author_email="nikhilshetty439@gmail.com",
+    packages= find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
---
+++ template.py
@@ -0,0 +1,46 @@
+import os, sys 
+from pathlib import Path
+import logging 
+
+while True:
+    project_name = input("Project Folder Name: ")
+    if project_name != "":
+        break
+
+crucks = [
+    f"{project_name}/__init__.py",
+    f"{project_name}/exception.py",
+    f"{project_name}/logger.py",
+    f"{project_name}/utils.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",
+    f"{project_name}/components/data_evaluation.py"
+    f"{project_name}/components/data_transformation.py"
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/components/model_evaluation.py",
+    f"{project_name}/components/model_pusher.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/entity/artifact_entity.py",
+    f"{project_name}/pipeline/__init__.py",
+    f"{project_name}/pipeline/training_pipeline.py",
+    f"{project_name}/pipeline/prediction_pipeline.py",
+    "app.py",
+    "setup.py"
+]
+
+for component in crucks:
+    file_path = Path(component)
+    file_dir, filename = os.path.split(file_path)
+
+    if file_dir != "":
+        os.makedirs(file_dir, exist_ok=True)
+        logging.info(f"Creating Directory {file_dir} / file: {filename}")
+
+    if not(os.path.exists(file_path)) or (os.path.getsize(file_path)==0):
+        with open(file_path, "w") as f:
+            pass 
+
+    else:
+        logging.info("File Structure Already Initiated")
+    
