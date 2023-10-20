7abe811a17fbcfaf1fd1cb0223131e001b117911
wiznikvibe <nikhilshetty439@gmail.com>
2023-10-20 21:25:05 +0530

    'Income_Classifier'

---
+++ src/logger.py
@@ -0,0 +1,16 @@
+from datetime import datetime 
+import os
+import logging 
+
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+log_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+
+os.makedirs(log_path, exist_ok=True)
+LOG_FILE_PATH = os.path.join(log_path, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
