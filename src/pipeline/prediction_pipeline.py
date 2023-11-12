07de614b9c9615aa579bc85ba1de2161b2077b1f
wiznikvibe <nikhilshetty439@gmail.com>
2023-11-12 18:29:48 +0530

    Build, Tag and Upload

--- src/pipeline/prediction_pipeline.py
+++ src/pipeline/prediction_pipeline.py
@@ -14,6 +14,7 @@ class PredictionPipeline:
 
     def predict(self, features):
         preprocessor_path = self.model_resolver.get_latest_transformer_path()
+        print(preprocessor_path)
         model_path = self.model_resolver.get_latest_model_path()
         
         preprocessor = load_object(file_dir=preprocessor_path)
